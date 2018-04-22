3345406306a0a1f1f7bb240bbeef3e8ad603568c
mseffner <mseffner@gmail.com>
2018-04-22 11:40:00 -0700

    added append

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -47,4 +47,9 @@ object List {
       if (p(h)) dropWhile(t, p)
       else as
   }
+
+  def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
+    case Nil => a2
+    case Cons(h, t) => Cons(h, append(t, a2))
+  }
 }
