633a52588cf3fc72aa564ec5f5132d766fe8069b
mseffner <mseffner@gmail.com>
2018-04-22 09:31:31 -0700

    added tests for tail

---
+++ src/test/scala/ListTests.scala
@@ -0,0 +1,13 @@
+import org.scalatest.FunSuite
+
+class ListTests extends FunSuite {
+
+  test("List.tail") {
+    val list1 = List(1, 2, 3, 4, 5)
+    val list2 = List(2, 3, 4, 5)
+    assert(List.tail(list1) === list2)
+    assert(List.tail(Nil) === Nil)
+    assert(List.tail(List(1)) === Nil)
+    assert(List.tail(List.tail(list1)) === List.tail(list2))
+  }
+}
