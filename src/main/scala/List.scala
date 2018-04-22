4b277709c788edd19e7d50e716763338f8e67d05
mseffner <mseffner@gmail.com>
2018-04-22 08:35:55 -0700

    fixed product and apply being outside List

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -7,14 +7,14 @@ object List {
     case Nil => 0
     case Cons(h, t) => h + sum(t)
   }
-}
 
-def product(ds: List[Double]): Double = ds match {
-  case Nil => 1.0
-  case Cons(0.0, _) => 0.0
-  case Cons(h, t) => h * product(t)
-}
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(h, t) => h * product(t)
+  }
 
-def apply[A](a: A*): List[A] =
-  if (a.isEmpty) Nil
-  else Cons(a.head, apply(a.tail: _*))
+  def apply[A](a: A*): List[A] =
+    if (a.isEmpty) Nil
+    else Cons(a.head, apply(a.tail: _*))
+}
