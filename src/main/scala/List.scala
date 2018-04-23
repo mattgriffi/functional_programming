7190ef04001714acd45ae3dd9f4a46c1d1f6565e
mseffner <mseffner@gmail.com>
2018-04-22 18:31:16 -0700

    zipWith Lists no longer have to be same type

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -156,7 +156,7 @@ object List {
   }
 
   /* Exercise 3.23 page 43 */
-  def zipWith[A, B](a1: List[A], a2: List[A])(f: (A, A) => B): List[B] = (a1, a2) match {
+  def zipWith[A, B, C](a1: List[A], a2: List[B])(f: (A, B) => C): List[C] = (a1, a2) match {
     case (Nil, _) => Nil
     case (_, Nil) => Nil
     case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
